ec8674a6b900c81f8b98febc3370ff23166df43e
Darren Ford <dford_au-reg@yahoo.com>
2024-05-11 10:43:05 +1000

    Fixed test build

--- Tests/AppKitFocusOverlayTests/AppKitFocusOverlayTests.swift
+++ Tests/AppKitFocusOverlayTests/AppKitFocusOverlayTests.swift
@@ -1,11 +1,8 @@
 import XCTest
-@testable import AppKitFocusPathDisplay
+@testable import AppKitFocusOverlay
 
-final class AppKitFocusPathDisplayTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AppKitFocusPathDisplay().text, "Hello, World!")
-    }
+final class AppKitFocusOverlayTests: XCTestCase {
+	func testExample() throws {
+		
+	}
 }
